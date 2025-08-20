d3cfe7a0cb07f7f275a73db709c7eecca26c8f47
Joaoglp <Joaoglp2507@gmail.com>
2025-08-20 10:29:59 +0100

    Update 20.08

--- client/src/App.js
+++ client/src/App.js
@@ -12,18 +12,12 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
+          <p></p>
         </header>
         <div>
+          <p></p>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
